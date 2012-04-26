59651a05c5dc6a421ae5a52e26783e2edce873bb
Tomasz Półgrabia <tpsa.s.a@gmail.com>
2012-04-26 15:28:42 +0200

    Inicjalizacja repo

---
+++ WpfTrainingHome/zadanie3/ColorConverter.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ColorConverter.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace zadanie3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class ColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter,
+           System.Globalization.CultureInfo culture)
+        {
+            double red = (double)values[0];
+            double green = (double)values[1];
+            double blue = (double)values[2];
+            return new SolidColorBrush(
+            Color.FromArgb(255, (byte)red, (byte)green, (byte)blue));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfTrainingHome/zadanie5/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace zadanie5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int angle = 15;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ContentWebBrowser.Source = new Uri(AddressTextBox.Text);
+            }
+            catch (UriFormatException Excp)
+            {
+                MessageBox.Show("Nieznany format");
+            }
+            catch (Exception Excp)
+            {
+                MessageBox.Show("Excp");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (ContentWebBrowser.CanGoBack)
+                ContentWebBrowser.GoBack();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (ContentWebBrowser.CanGoForward)
+                ContentWebBrowser.GoForward();
+        }
+    }
+}
